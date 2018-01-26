c22b3b90a8c91816c686a1602868c171aa15a6d3
smb2255 <smb2255@columbia.edu>
2018-01-26 15:15:04 -0500

    Got the 'create' run request to work successfully.

--- scripts/runs/create.sh
+++ scripts/runs/create.sh
@@ -2,11 +2,10 @@ curl "http://localhost:4741/runs" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
-  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "run": {
       "date": "'"${DATE}"'",
-      "distance": "'"${DIST}"'",
-      "time": "'"${DIAG}"'",
+      "distance": "'"${DISTANCE}"'",
+      "time": "'"${TIME}"'"
     }
   }'
